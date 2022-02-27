c84386ac03c5f98a406c97022d214a026c0d818f
xso2 <odaken1002@gmail.com>
2022-02-27 12:40:31 +0900

    ユーザー管理機能

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise  :database_authenticatable, :registerable,
+          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+  validates :age , presence: true
+  validates :height , presence: true
+  validates :weight , presence: true
+
+end
