0475744a011edba07a388ddf3ee7381ed8d771eb
xso2 <odaken1002@gmail.com>
2022-03-09 15:14:58 +0900

    Update unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,7 +2,7 @@
 app_path = File.expand_path('../../../', __FILE__)
 
 #アプリケーションサーバの性能を決定する
-worker_processes 2
+worker_processes 1
 
 #アプリケーションの設置されているディレクトリを指定
 working_directory "#{app_path}/current"
